70daed0f92b9671eb7e5aed5d582dcd2126effdf
Leon <leon.domagalski@me.com>
2024-04-22 16:28:34 +0200

    scraper_class
    added documentation to scraper class, implemented writing to an excel file, not tested yet

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from modules import Scraper
+
+def main():
+    
+    url_list = [
+        "https://ucalendar.uwaterloo.ca/2223/COURSE/course-AMATH.html",
+        "https://ucalendar.uwaterloo.ca/2223/COURSE/course-CO.html",
+        "https://ucalendar.uwaterloo.ca/2324/COURSE/course-STAT.html",
+        "https://ucalendar.uwaterloo.ca/2324/COURSE/course-ACTSC.html",
+        "https://ucalendar.uwaterloo.ca/2324/COURSE/course-CS.html",
+        "https://ucalendar.uwaterloo.ca/2324/COURSE/course-PHYS.html",
+        "https://ucalendar.uwaterloo.ca/2223/COURSE/course-PMATH.html",
+        "https://ucalendar.uwaterloo.ca/2324/COURSE/course-MATH.html",
+        "https://ucalendar.uwaterloo.ca/2324/COURSE/course-MTHEL.html"
+
+        # TODO: Add course decriptions manually for the following courses: BIOL 239, HLTH 101, ENGL 378, AFM 424 
+        # TODO: go through the rest of the majors and find courses that don't fit
+
+    ]
+
+    scraper = Scraper()
+
+    for url in url_list:
+        scraper.get_courses_with_description(url=url)
+    
+    scraper.write_to_file()
+
+    # Not tested yet
+
+
+if __name__ == "__main__":
+    main()
--- src/modules/scraper_class.py
+++ src/modules/scraper_class.py
@@ -1,15 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
+import xlsxwriter
 
 class Scraper:
-    def __init__(self):
+    def __init__(self, ):
         self.service = webdriver.ChromeService()
         self.driver = webdriver.Chrome(service=self.service)
+
         self.course_list = []
 
     def strip_name(self, course: str) -> str:
+        """ 
+        Removes session type labels from the course name
+        
+        Args:
+            course: A string containing the course name and possibly additional 
+                labels and numeric information.
 
+        Returns:
+            A string with session type labels and spaces removed from the original
+            course name.
+
+        """
         pattern = r'\s+(LEC|TUT|TST|LAB)(,\s*(LEC|TUT|TST|LAB))*\s*\d*\.?\d*'
         cleaned_course_name = re.sub(pattern, '', course)
 
@@ -17,8 +30,18 @@ class Scraper:
 
         return final_name
     
-    def get_courses_with_description(self, url: str) -> list[dict]:
+    def get_courses_with_description(self, url: str):
+        """
+        Function that takes in a url to the math courses section of the
+        uwaterloo undergrad calendar and adds courses, along with their description
+        to the self.course_list
 
+        Args:
+            url: string: the url of the uw ug calendar containing the courses
+
+        Returns: 
+            None
+        """
         self.driver.get(url)
 
         courses = self.driver.find_elements(By.CSS_SELECTOR, 'div.divTable')
@@ -30,7 +53,7 @@ class Scraper:
             except IndexError:
                 course_description = "No detailed description available."
 
-            self.course_list.append({"course_name": self.strip_name(course_name), "course_description": course_description, "course_diff": 0})
+            self.course_list.append({"course_name": self.strip_name(course_name), "course_description": course_description, "course_difficulty": 0})
 
             print("Course Name:", self.strip_name(course_name))
             print("Course Description:", course_description)
@@ -38,9 +61,42 @@ class Scraper:
         
         self.driver.quit()
 
-        return course_list
+    def get_course_difficulties(self, url: str):
+        ...
 
-    def get_course_difficulties(self, url)
+    
+    def write_to_file(self, file_name: str):
+        """Writes course data to an Excel file.This method takes course 
+        information stored within the instance's `course_list` attribute 
+        and writes it to an Excel file using `xlsxwriter`. 
+    
+    Args:
+        file_name: A string representing the name of the file to which the Excel data will be written
+
+    Returns:
+        None
+
+    """
+        workbook = xlsxwriter.Workbook(f"{file_name}.xslx")
+        worksheet = workbook.add_worksheet()
+
+        bold = workbook.add_format({'bold': True})
+
+        worksheet.write("A1", "course_name", bold)
+        worksheet.write("B1", "course_description", bold)
+        worksheet.write("C1", "course_difficulty", bold)
+
+        row = 1
+        col = 0
+
+        for course in self.course_list:
+            worksheet.write(row, col, course["course_name"])
+            worksheet.write(row, col + 1, course["course_description"])
+            worksheet.write(row, col + 2, course["course_difficulty"])
+            row += 1
+        
+        workbook.close()
+
 
 scraper = Scraper()
 scraper.get_courses_with_description(url="https://ucalendar.uwaterloo.ca/2324/COURSE/course-MATH.html")
